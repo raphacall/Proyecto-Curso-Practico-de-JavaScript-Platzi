c62f1bc7fed38b19dfc6734174df609ab3b4974b
raphacall <raphacall@gmail.com>
2023-09-12 22:20:18 -0400

    integracion del proyecto de Frontend Developer

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-.console.log('Hello, world');
+const menuBox = document.querySelector('.menu-box');
+const email = document.querySelector('.email-address');
+
+email.addEventListener('click', () => {
+  menuBox.classList.toggle('inactive');
+});
