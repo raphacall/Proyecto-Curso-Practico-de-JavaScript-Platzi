f34849eda9c270c2e9f24ecdfdcfeeb535e96879
raphacall <raphacall@gmail.com>
2023-09-13 06:32:19 -0400

    navbar agregado

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const menuBox = document.querySelector('.menu-box');
 const email = document.querySelector('.email-address');
+const menuMobileIcon = document.querySelector('.menu-icon');
+const menuMobile = document.querySelector('.menu-box-mobile');
 
 email.addEventListener('click', () => {
   menuBox.classList.toggle('inactive');
 });
+
+menuMobileIcon.addEventListener('click', () => {
+    menuMobile.classList.toggle('inactive');
+})
