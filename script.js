65ec86e98338bc4d1f3f12176d57fc46e389b5cb
raphacall <raphacall@gmail.com>
2023-09-14 21:49:08 -0400

    Shopping cart page integrado.

--- script.js
+++ script.js
@@ -2,11 +2,36 @@ const menuBox = document.querySelector('.menu-box');
 const email = document.querySelector('.email-address');
 const menuMobileIcon = document.querySelector('.menu-icon');
 const menuMobile = document.querySelector('.menu-box-mobile');
+const shoppingCart = document.querySelector('.cart-icon');
+const shoppingContainer = document.querySelector('.shopping-container');
+const shoppingCloseArrow = document.querySelector('#shopping-close-arrow');
 
 email.addEventListener('click', () => {
   menuBox.classList.toggle('inactive');
+  if (!shoppingContainer.classList.contains('inactive')) {
+      shoppingContainer.classList.toggle('inactive');
+  }
 });
 
 menuMobileIcon.addEventListener('click', () => {
     menuMobile.classList.toggle('inactive');
+    if (!shoppingContainer.classList.contains('inactive')) {
+        shoppingContainer.classList.toggle('inactive');
+    }
+})
+
+shoppingCart.addEventListener('click', () => {
+    shoppingContainer.classList.toggle('inactive');
+    if (!menuMobile.classList.contains('inactive')) {
+        menuMobile.classList.toggle('inactive');
+    }
+    if (!menuBox.classList.contains('inactive')) {
+        menuBox.classList.toggle('inactive');
+    }
+})
+shoppingCloseArrow.addEventListener('click', () => {
+  shoppingContainer.classList.toggle('inactive');
+  if (!menuMobile.classList.contains('inactive')) {
+      menuMobile.classList.toggle('inactive');
+  }
 })
